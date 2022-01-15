48391b6ef3706f07e43d01d5cd515869a06f0018
Katie <costantinkp@gmail.com>
2022-01-15 08:48:21 -0500

    finished functionality

--- src/Hours.js
+++ src/Hours.js
@@ -10,46 +10,69 @@ export const Hours = () => {
     console.log(endTime);
     console.log(hourly);    
 
-    const toStart = () => {
+    const calculate = () => {
        const x = +startTime.search(":")
        const sHour = +startTime.slice(0,x)
        const sHourToMin = +sHour*60;
        const sMin = +startTime.slice(x+1)
        const start = sHourToMin+sMin;
-       console.log(start);
+       //console.log(start);
+
+       const y = +endTime.search(":")
+       const eHour = +endTime.slice(0,y)
+       const eHourToMin = +eHour*60;
+       const eMin = +endTime.slice(y+1)
+       const end = eHourToMin+eMin;
+       //console.log(end);
+
+       const minsWorked = end - start
+       const hoursWorked = minsWorked/60;
+       //console.log(hoursWorked);
+
+       const posHoursWorked = hoursWorked < 0 ? hoursWorked+24 : hoursWorked;
+       
+       const pay = posHoursWorked*(+hourly);
+       return pay;
+
     };
+    const answer = calculate();
 
 
     return (
         
        <>
-            <label htmlFor="startTime">Start Time:</label>
-            <input 
-                type="time" 
-                name="startTime" 
-                id="startTime" 
-                onChange={ (e) => setStartTime(e.target.value)}
-            />
-    
-            <label htmlFor="endTime">End Time:</label>
-            
-            <input 
-                type="time" 
-                name="endTime" 
-                id="endTime" 
-                onChange={ (e) => setEndTime(e.target.value)}
-            />
-    
-            <label htmlFor="hourly">Hourly:</label>
-            
-            <input 
-                type="number" 
-                name="hourly" 
-                id="hourly" 
-                onChange={ (e) => setHourly(e.target.value)}
-            />
-            
-            <button onClick={toStart}> submit </button>
+  <div className='calcDiv'>
+                <label htmlFor="startTime">Start Time:</label>
+                <input 
+                    type="time" 
+                    name="startTime" 
+                    id="startTime" 
+                    onChange={ (e) => setStartTime(e.target.value)}
+                />
+        
+                <label htmlFor="endTime">End Time:</label>
+                
+                <input 
+                    type="time" 
+                    name="endTime" 
+                    id="endTime" 
+                    onChange={ (e) => setEndTime(e.target.value)}
+                />
+        
+                <label htmlFor="hourly">Hourly:</label>
+                
+                <input 
+                    type="number" 
+                    name="hourly" 
+                    id="hourly" 
+                    onChange={ (e) => setHourly(e.target.value)}
+                />
+                
+                <button onClick={calculate}> submit </button>
+  </div>
+  <div className='answdiv'>
+      pay: {answer}
+  </div>
        </>
     )
 }
